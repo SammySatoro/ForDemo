ac9e0d97fd73ca0ba649c3ed371a321d9f02f98b
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-04-12 22:58:56 +0300

    Add files via upload

---
+++ FS_5/FS_5/lw_11.fs
@@ -0,0 +1,17 @@
+﻿module lw11
+open System
+
+let processAnswer  answer = 
+    match answer with
+        |"F#"|"Prolog"-> "Sycophant!"
+        |"C++"|"c++"|"Cxx"|"cxx"|"CXX"-> "Just don't talk to me anymore..."
+        |"C#"-> "Already tried Unity?"
+        |"JavaScript"|"javascript"-> "It ain't Java, you know that?"
+        |"Java"-> "I've played Minecraft too"
+        |"Python" -> "..."
+        |other -> "You're goddamn psychopath!"
+    
+let Demo =
+    printf "Which programming language do you like the most?: " 
+    let answer = Console.ReadLine()
+    Console.WriteLine(processAnswer answer)
