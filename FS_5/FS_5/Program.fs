2ba7929a251ad2e2e8980b48ed374d5dac8c20d8
SammySatoro <80969335+SammySatoro@users.noreply.github.com>
2022-04-12 23:11:04 +0300

    Add files via upload

---
+++ FS_5/FS_5/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+
+
+[<EntryPoint>]
+let main argv =
+    //lw11.Demo
+    //lw12.Demo
+    //lw13.Demo
+    //lw14.Demo
+    //lw15.Demo
+    //lw16.Demo
+    //lw17.Demo
+    //lw18.Demo
+    //lw19.Demo
+    //lw20.Demo
+    0
