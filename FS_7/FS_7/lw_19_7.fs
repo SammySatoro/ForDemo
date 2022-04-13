ba1996afb80142978da8c60319aa46451c113d16
SammySatoro <ivzhenko2014@gmail.com>
2022-04-13 17:02:35 +0300

    Fixed lw_19_7

--- FS_7/FS_7/lw_19_7.fs
+++ FS_7/FS_7/lw_19_7.fs
@@ -4,9 +4,11 @@ let filterUppercase str = str |> String.filter (fun i -> i > 'A' && i < 'Z')
    
 let isPalindrome (str:string) =
     let charArray = str.ToCharArray() 
+    printfn "%A" charArray
     let reversedCharArray = Array.rev charArray  
-    (charArray , reversedCharArray) 
-    ||> Array.fold2 (fun is c r -> if c = r then true else false) false
+    printfn "%A" reversedCharArray
+    ((charArray , reversedCharArray) ||> Array.fold2 (fun is c r -> if c = r then is + 0 else is + 1) 0) = 0
+    
 
 let ForDemo19_7 = 
-    printfn "%A" (isPalindrome (filterUppercase "wOerOtLyOO")) //OOLOO true
+    printfn "%A" (isPalindrome (filterUppercase "LO0LwOeLOrtLyO")) //OOLOO true
