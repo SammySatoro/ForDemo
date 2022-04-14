d9d8405eab527b67df2565d69f125d229bedc4af
SammySatoro <ivzhenko2014@gmail.com>
2022-04-15 02:01:21 +0300

    Pushed lw_20_5

--- FS_7/FS_7/lw_20_5.fs
+++ FS_7/FS_7/lw_20_5.fs
@@ -1,28 +1,50 @@
 ﻿module lw_20_5
 
-let ratingOfLetters = [|("a", 8.17); ("b", 1.49); ("c", 2.78); ("d", 4.25); ("e", 12.68); ("f", 2.23); 
-                        ("g", 2.02); ("h", 6.09); ("i", 6.97); ("j", 0.15); ("k", 0.77); ("l", 4.03); ("m", 2.41); ("n", 6.75); 
-                        ("o", 7.51); ("p", 1.93); ("q", 0.1); ("r", 5.99);("s", 6.33); ("t", 9.06); ("u", 2.76); ("v", 0.98); 
-                        ("w", 2.36); ("x", 0.15); ("y", 1.97); ("z", 0.07)|]  
+let ratingInAlphabet = [|8.17; 1.49; 2.78; 4.25; 12.7; 2.23; 2.02; 6.09; 6.97; 0.15; 0.77; 4.03; 2.41;                                           
+                                      6.75; 7.51; 1.93; 0.1; 5.99; 6.33; 9.06; 2.76; 0.98; 2.36; 0.15; 1.97; 0.07|] 
 
-let lines = [|"qwedgdhfg hh fgh fdjhdfhfh fa";
-            "gdfgfs fadgdfg dshg fghf hf";
-            "hfghsfghfghh fghsfh fsghfgh";
-            "hfgshfjf jgdhjgj gdhj hgjj "|]
-   
-let quantifyTheMostFrequent (linesArray:array<string>) =
-    let arraysWithoutSpaces = linesArray |> Array.map (fun line -> String.filter (fun c -> c <> ' ') line)
-    let numbersOfChars = arraysWithoutSpaces |> Array.map (fun (line:string) -> line.Length)
-    let arrayOfArraysOfChars = arraysWithoutSpaces |> Array.map (fun line -> line.ToCharArray())
-    let arratOfArraysOfRates = arrayOfArraysOfChars |> Array.map (fun line -> line |> Array.countBy (fun i -> i) |> Array.sortByDescending snd)
-    let pairsOfMostRated = arratOfArraysOfRates |> Array.map (fun arr -> arr.[0])
-    pairsOfMostRated, numbersOfChars
+let lines = [|"gg sh hjfsjgh fgh fsgh";
+                    "afsag vxcz bhgf dhf dfgad";
+                    "gg s gh hjffsdf";
+                    "nvbxgf sh hjfsjghfds fgh fsa gh";
+                    "fdg gg s hh hjhfh fsjg hfghh fgh fsgh";
+                    "siuf sfsdnf sgaoidngu";
+                    "qho fosdhfo igjgfgf oig";
+                    "q pofipfsafgurgjg gdjg  fg d g";|]
+  
+let rateInAlpOfMostRatedLetter (char:char)= 
+     if (System.Char.IsLower char) then ratingInAlphabet.[(int)char - 97]   
+     else ratingInAlphabet.[(int)char- 65]
 
-let quantifyEntryPercentage (pairs:array<char * int>) (entryNumbers:array<int>) =
-    let freqs = (pairs, entryNumbers) ||> Array.map2 (fun (pair:char * int) number -> (fst pair ,((float)(snd pair) / (float)number) * 100.0))
-    freqs
+let findQuadraticDeviations (linesArray:array<string>) =
+    let arrayOfArraysOfChars = linesArray |> Array.map (fun line -> line.ToCharArray())    
+    let arrayOfNumbersOfTheMostRatedChars  =  
+        arrayOfArraysOfChars 
+        |> Array.map (fun line -> line |> Array.countBy (fun i -> i)  |> Array.sortByDescending snd) |> Array.map (fun pair -> Array.head pair)
+
+    let arrayOfRatingsOfTheMostRatedInLines = 
+        (arrayOfNumbersOfTheMostRatedChars, linesArray)
+        ||> Array.map2 (fun (pair:char*int) (line:string) -> (float)(snd pair) / (float)(String.length line) * 100.0) 
+
+    let arrayOfRatingsOfTheMostRatedInAlphabet =
+        arrayOfNumbersOfTheMostRatedChars
+        |> Array.map (fun pair -> rateInAlpOfMostRatedLetter (fst pair))
+
+    let arrayOfQuadraticDeviations =
+        (arrayOfRatingsOfTheMostRatedInAlphabet, arrayOfRatingsOfTheMostRatedInLines)
+        ||> Array.map2 (fun rateAlph rateLine -> (rateAlph - rateLine)*(rateAlph - rateLine))
+
+    arrayOfQuadraticDeviations
+  
+let sortByQuadraticDeviations strings =
+    let pairsStringQD =
+        (strings, findQuadraticDeviations strings)
+        ||> Array.map2 (fun str qd -> (str, qd))
+    Array.sortBy snd pairsStringQD |> Array.map (fun pair -> fst pair) 
  
-
-let ForDemo =
-    printfn "%A" (quantifyTheMostFrequent lines)
-    printfn "%A" (quantifyEntryPercentage (fst (quantifyTheMostFrequent lines)) (snd (quantifyTheMostFrequent lines)))
+let printStrings strings = 
+    strings |> Array.map (fun str -> printfn "%A" str)
+        
+let ForDemo20_5 =
+    printStrings(sortByQuadraticDeviations lines)
+    
