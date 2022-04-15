3fbcbbb91875b526ff23bbe98e4323e2efb3da19
SammySatoro <ivzhenko2014@gmail.com>
2022-04-15 18:32:04 +0300

    Pushed lw_20_8

--- FS_7/FS_7/lw_20_8.fs
+++ FS_7/FS_7/lw_20_8.fs
@@ -1,24 +1,35 @@
 ﻿module lw_20_8
 
-let rateInAlp = [|8.17; 1.49; 2.78; 4.25; 12.7; 2.23; 2.02; 6.09; 6.97; 0.15; 0.77; 4.03; 2.41;                                           
-                          6.75; 7.51; 1.93; 0.1; 5.99; 6.33; 9.06; 2.76; 0.98; 2.36; 0.15; 1.97; 0.07|] 
+let lines = [|  "gg sh hjfsjgh fgh fsgh";
+                    "afsag vxcz bhgf dhf dfgad";
+                    "gg s gh hjffsdf";
+                    "nvbxgf sh hjfsjghfds fgh fsa gh";
+                    "fdg gg s hh hjhfh fsjg hfghh fgh fsgh";
+                    "siuf sfsdnf sgaoidngu";
+                    "qho fosdhfo igjgfgf oig";
+                    "q pofipfsafgurgjg gdjg  fg d g";|]
 
-let lines = [|"    ";
-                   "zzz"|]
+let findAverageWeight (str:string) =
+    (float)([|0 .. (String.length str) - 1|] 
+    |> Array.map (fun ind -> (int)str.[ind]) 
+    |> Array.sum) 
+    / (float)(String.length str)
 
-let rateOfMostRatedLetter = 
-    (((String.filter (fun c -> c = 'a') "abcd").Length |> System.Convert.ToDouble)  / ( "abcd".Length |> System.Convert.ToDouble)) * 100.0
+let findMaxAverageTriple (str:string) =
+    (float)(([|0 .. (String.length str) - 3|], [|1 .. (String.length str) - 2|], [|2 .. (String.length str) - 1|]) 
+    |||> Array.map3 (fun i1 i2 i3 ->  (int)(str.[i1]) + (int)(str.[i2]) + (int)(str.[i3]))
+    |> Array.max)
+    / (float)(String.length str)
 
-let rateInAlpOfMostRatedLetter = 
-    if (System.Char.IsLower 'A') 
-    then rateInAlp.[(System.Convert.ToInt32('A') - 97)]
-    else rateInAlp.[(System.Convert.ToInt32('A') - 65)]
-
+let findQuadraticDeviation (str:string) = (findAverageWeight str - findMaxAverageTriple str) ** 2.0
+    
+let sortByQuadraticDeviation (linesArray:array<string>) = 
+    linesArray |> Array.sortBy (fun line -> findQuadraticDeviation line)
+ 
+let printStrings strings = 
+    strings |> Array.map (fun str -> printfn "%A" str)
+ 
 let ForDemo20_8 =
-    printfn "%A" (rateInAlp)
+    printStrings (sortByQuadraticDeviation lines)
 
 
-
-//В порядке увеличения квадратического отклонения
-//(частоты встречаемости самого часто всречаемого в строке символа) – mostRatedInString
-//от (частоты его встречаемости в текстах на этом алфавите) – rateInAlphabet
