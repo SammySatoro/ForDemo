ece604be83936c15e36d0b260729a7e88bb4edbd
SammySatoro <ivzhenko2014@gmail.com>
2022-04-20 03:04:39 +0300

    9

---
+++ FS_9/FS_9/Program.fs
@@ -0,0 +1,15 @@
+﻿open System.Windows.Forms
+
+let mainForm = 
+    let main = new Form(Text = "Parent form" ,Width = 480, Height = 360)
+    let button5 = new Button(Dock = DockStyle.Top, Text = "lw_5")
+
+    let openForm5  =  lw_5.form.ShowDialog() |> ignore
+
+    button5.Click.Add(fun e -> openForm5)
+
+
+    main.Controls.Add(button5)
+
+    do Application.Run(main)
+
